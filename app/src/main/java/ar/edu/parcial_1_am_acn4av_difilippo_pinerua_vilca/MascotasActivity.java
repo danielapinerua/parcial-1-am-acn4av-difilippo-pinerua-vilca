14d094c04d034006a2ca609db44397aae0d40202
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-09-12 00:38:59 -0300

    feat: add navigation to mascot screen

--- app/src/main/java/ar/edu/parcial_1_am_acn4av_difilippo_pinerua_vilca/MascotasActivity.java
+++ app/src/main/java/ar/edu/parcial_1_am_acn4av_difilippo_pinerua_vilca/MascotasActivity.java
@@ -7,26 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MascotasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button_ver_mascotas);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MascotasActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_mascotas);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
