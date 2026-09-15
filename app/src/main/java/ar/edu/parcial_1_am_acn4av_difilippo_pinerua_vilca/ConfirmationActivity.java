7060492364d64884af93cc2c40fa75c1f56d6602
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-09-14 21:51:01 -0300

    feat: add adoption confirmation screen

--- app/src/main/java/ar/edu/parcial_1_am_acn4av_difilippo_pinerua_vilca/ConfirmationActivity.java
+++ app/src/main/java/ar/edu/parcial_1_am_acn4av_difilippo_pinerua_vilca/ConfirmationActivity.java
@@ -1,7 +1,8 @@
 package ar.edu.parcial_1_am_acn4av_difilippo_pinerua_vilca;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,6 +16,12 @@ public class ConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_confirmation);
+        Button btnBackToPets = findViewById(R.id.btn_back_to_pets);
+        btnBackToPets.setOnClickListener(v -> {
+            Intent intent = new Intent(ConfirmationActivity.this, PetListActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
